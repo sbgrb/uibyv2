697c8374e269f9fbc106070e531176efa7f19e3f
刘坤 <liuk@yeyoo.cn>
2022-03-09 23:03:56 +0800

    新增input及样式

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import Button from "./button";
 import icon from "./icon";
+import input from "./input";
 
 Vue.component('g-button',Button)
 Vue.component('g-icon',icon)
+Vue.component('g-input',input)
 new Vue({
     el:'#app',
 })
